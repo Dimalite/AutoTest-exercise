82ef89d828e5612cef686e2fac7fbfd7185762b9
DimaLite <80548053+Dimalite@users.noreply.github.com>
2021-11-22 14:30:08 +0300

    modified

---
+++ test/1_openPage.js
@@ -0,0 +1,12 @@
+// const { Builder, By, Key } = require('selenium-webdriver');
+
+// describe("Checking login page", function () {
+  
+//   it('Open the page', async function () {
+
+//     let driver = await new Builder().forBrowser("firefox").build();
+//     await driver.get("https://www.pecodesoftware.com/qa-portal/registerlogin/registerlogin.php");
+
+//     await driver.quit();
+//   })
+// })
---
+++ test/2_credential.js
@@ -0,0 +1,25 @@
+// const {
+//   Builder,
+//   By,
+//   Key
+// } = require('selenium-webdriver');
+
+
+// describe("Checking login page", function () {
+
+//   it('Credential values.', async function () {
+
+//     let driver = await new Builder().forBrowser("firefox").build();
+//     await driver.get("https://www.pecodesoftware.com/qa-portal/registerlogin/registerlogin.php");
+
+//     let inputName = await driver.findElement(By.name("username")).getAttribute("value").then(function (value) {
+//       return value;
+//     });
+
+//     let inputPassword = await driver.findElement(By.name("password")).getAttribute("value").then(function (value) {
+//     return value;
+//     });
+    
+//     await driver.quit();
+//   })
+// })
